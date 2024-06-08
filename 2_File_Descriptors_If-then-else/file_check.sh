f69653ea122dcaab57948a8cd1ea4037de1c036a
Savana Rohit <rohitsavana18@gmail.com>
2024-06-08 08:22:29 +0530

    Add File_Descriptors_If-then-else dir

--- 2_File_Descriptors_If-then-else/file_check.sh
+++ 2_File_Descriptors_If-then-else/file_check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Script to check if the passwd file is present or not
 if [[ -e "/etc/passwd" ]];
-then 
-	echo "passwd file exists"
+then
+    echo "passwd file exists"
 else
-	echo "File not found"
+    echo "File not found"
 fi
